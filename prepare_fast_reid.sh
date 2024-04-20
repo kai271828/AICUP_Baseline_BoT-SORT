34953840f71da1f493ae78e09e62ace5a8e1a607
kai <40947038S@ntnu.edu.tw>
2024-04-20 11:23:53 +0800

    update

--- prepare_fast_reid.sh
+++ prepare_fast_reid.sh
@@ -1,16 +1,20 @@
-pip install -r requirements.txt
-pip install cython; pip3 install -q 'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI'
-pip install yacs
+#!/bin/bash
+zip_file="./32_33_train_v2.zip"
+
+pip install -qr requirements.txt
+pip install -q cython; pip3 install -q 'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI'
 # Cython-bbox
-pip install cython_bbox
+pip -q install cython_bbox
 
 # faiss cpu / gpu
 # !pip install -q faiss-cpu
-pip install faiss-gpu
+pip install -q faiss-gpu
 
 # Download the AI_CUP dataset
-gdown 1zfiIB7r_pbFgLzXlkdhmW7LrWqFDDlZy
-unzip /content/AICUP_Baseline_BoT-SORT/32_33_train_v2.zip
+if [ ! -f "$zip_file" ]; then
+  gdown 1zfiIB7r_pbFgLzXlkdhmW7LrWqFDDlZy
+fi
+unzip -n ./32_33_train_v2.zip
 
 # prepare ReID dataset
 python fast_reid/datasets/generate_AICUP_patches.py --data_path /content/AICUP_Baseline_BoT-SORT/train
